4277145c2e54ef379b025ea96597ef62443ebedc
shoffandarul <shoffan.darul.mufti.tik21@mhsw.pnj.ac.id>
2022-02-12 15:38:25 +0700

    belajar assignment operator dan for loop

---
+++ 12 - For Loop/main.py
@@ -0,0 +1,37 @@
+# For Loop
+
+"""
+    for kondisi:
+        aksi
+"""
+
+angkaangka = [0,2,4,6,8] # ini adalah list
+print(angkaangka)
+
+for i in angkaangka:
+    print(f"i sekarang -> {i}")
+
+print("Akhir dari program 1\n")
+
+angka = range(5) # ini adalah range
+
+for i in angka:
+    print(f"i sekarang -> {i}")
+
+print("Akhir dari program 2\n")
+
+angka = range(1,10) # ini adalah range
+
+for i in angka:
+    print(f"i sekarang -> {i}")
+
+print("Akhir dari program 3\n")
+
+#menggunakan string data
+
+teks = "saya ganteeeeng"
+
+for huruf in teks:
+    print(huruf)
+
+print("Akhir dari program 4\n")
